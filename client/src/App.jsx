9f885478c7aff4d914745ec6eb5ccce52d417efc
jjwstin <justnotrightxie@gmail.com>
2025-02-22 23:49:56 -0500

    Entire Login Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/header';
+import Login from './pages/Login';
 import HomeScreen from './pages/HomeScreen';
 import ChatPage from './pages/ChatPage';
 
@@ -10,7 +11,8 @@ function App() {
     <Router>
       <Header />
       <Routes>
-        <Route path="/" element={<HomeScreen />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<HomeScreen />} />
         <Route path="/chat" element={<ChatPage />} />
       </Routes>
     </Router>
